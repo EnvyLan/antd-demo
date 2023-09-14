5275337215d91f7c0b86699118313a96c48bbc4f
Jiawei <604264001@qq.com>
2023-09-14 17:49:54 +0800

    commit topGun UI code

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import Link from "next/Link";
+
+export default function Page(){
+    return (
+        <>
+            <h1>Hello About</h1>
+            <Link href='/'>Go to Index</Link>
+        </>
+    )
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import MainPage from "./test"
-  
-
-export default function Home() {
-  return (
-    <MainPage />
-  )
-}
